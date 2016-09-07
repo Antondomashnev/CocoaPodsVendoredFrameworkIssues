746f268087eda47f29b0395370846f6d00536269
Anton Domashnev <antondomashnev@gmail.com>
2016-09-07 14:53:45 +0200

    Initial Commit

---
+++ test_podspec/test_podspec.h
@@ -0,0 +1,19 @@
+//
+//  test_podspec.h
+//  test_podspec
+//
+//  Created by Anton Domashnev on 07/09/16.
+//  Copyright © 2016 antondomashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for test_podspec.
+FOUNDATION_EXPORT double test_podspecVersionNumber;
+
+//! Project version string for test_podspec.
+FOUNDATION_EXPORT const unsigned char test_podspecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test_podspec/PublicHeader.h>
+
+
